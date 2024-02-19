94101c9a6abbeb2047d58c99c8ebedfaf2468524
Midhun MP <midhunmp7@gmail.com>
2024-02-19 15:25:50 +0530

    Step drawing logics updated with painter

--- lib/src/progress_step_with_blunt_chevron.dart
+++ lib/src/progress_step_with_blunt_chevron.dart
@@ -1,35 +1,47 @@
 import 'package:flutter/material.dart';
 
-import 'clippers/chevron_blunt_clipper.dart';
+import '../progress_stepper.dart';
+import 'shape/chevron_blunt.dart';
 
-class ProgressStepWithBluntChevron extends StatelessWidget {
+class ProgressStepWithBluntChevron extends StatelessWidget with ChevronBlunt {
   const ProgressStepWithBluntChevron({
     required double width,
+    required double height,
     required Color defaultColor,
     required Color progressColor,
+    Color borderColor = Colors.black,
+    double borderWidth = 0.0,
     required bool wasCompleted,
     Widget? child,
     Key? key,
   })  : _width = width,
+        _height = height,
         _defaultColor = defaultColor,
         _progressColor = progressColor,
+        _borderColor = borderColor,
+        _borderWidth = borderWidth,
         _wasCompleted = wasCompleted,
         _child = child,
         super(key: key);
 
   final double _width;
+  final double _height;
   final Color _defaultColor;
   final Color _progressColor;
+  final Color _borderColor;
+  final double _borderWidth;
   final bool _wasCompleted;
   final Widget? _child;
 
   @override
-  Widget build(BuildContext context) => ClipPath(
-        clipper: ChevronBluntClipper(),
-        child: Container(
-          width: _width,
-          color: _wasCompleted ? _progressColor : _defaultColor,
-          child: _child ?? Container(),
-        ),
-      );
+  Widget build(BuildContext context) => CustomPaint(
+    painter:
+    StepShapePainter(
+      path: getChevronBluntPath(Size(_width, _height)),
+      fillColor: _wasCompleted ? _progressColor : _defaultColor,
+      borderColor: _borderColor,
+      borderWidth: _borderWidth,
+    ),
+    child: _child,
+  );
 }
--- lib/src/progress_step_with_chevron.dart
+++ lib/src/progress_step_with_chevron.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
+import '../progress_stepper.dart';
+import 'shape/chevron_crumb.dart';
 
-import 'clippers/chevron_crumb_clipper.dart';
-
-class ProgressStepWithChevron extends StatelessWidget {
+class ProgressStepWithChevron extends StatelessWidget with Chevron {
   const ProgressStepWithChevron({
     required double width,
+    required double height,
     required Color defaultColor,
     required Color progressColor,
+    Color borderColor = Colors.black,
+    double borderWidth = 0.0,
     required bool wasCompleted,
     Widget? child,
     Key? key,
   })  : _width = width,
+        _height = height,
         _defaultColor = defaultColor,
         _progressColor = progressColor,
+        _borderColor = borderColor,
+        _borderWidth = borderWidth,
         _wasCompleted = wasCompleted,
         _child = child,
         super(key: key);
 
   final double _width;
+  final double _height;
   final Color _defaultColor;
   final Color _progressColor;
+  final Color _borderColor;
+  final double _borderWidth;
   final bool _wasCompleted;
   final Widget? _child;
 
   @override
-  Widget build(BuildContext context) => ClipPath(
-        clipper: ChevronClipper(),
-        child: Container(
-          width: _width,
-          color: _wasCompleted ? _progressColor : _defaultColor,
-          child: _child ?? Container(),
-        ),
-      );
+  Widget build(BuildContext context) => CustomPaint(
+    painter:
+    StepShapePainter(
+      path: getChevronPath(Size(_width, _height)),
+      fillColor: _wasCompleted ? _progressColor : _defaultColor,
+      borderColor: _borderColor,
+      borderWidth: _borderWidth,
+    ),
+    child: _child,
+  );
 }
